47fe5fa4d463fad93472abbf75c440be03bb72a2
Jan M. D. Hansen <jan.m.d.hansen@protonmail.com>
2025-05-17 16:13:09 +0200

    Delete .config/nvim/lua directory

--- .config/nvim/lua/plugin_config/init.lua
+++
@@ -1,5 +0,0 @@
-require("plugin_config.telescope")
-require("plugin_config.treesitter")
-require("plugin_config.lsp")
-require("plugin_config.cmp")
-require("plugin_config.plantuml")
--- .config/nvim/lua/plugin_config/plantuml.lua
+++
@@ -1,1 +0,0 @@
-require'soil'.setup{ }
